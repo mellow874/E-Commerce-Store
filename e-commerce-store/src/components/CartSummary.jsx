82f3e566f452ce4fa0f024e2d1d4f71d77fd3c36
mellow874 <felicitymncube49@gmail.com>
2025-11-14 12:06:03 +0200

    Cart syntax

--- e-commerce-store/src/components/CartSummary.jsx
+++ e-commerce-store/src/components/CartSummary.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
 
-function CartSummary() {
+import React from "react";
+// Import product images
+import Laptop from "../assets/Laptop.png";
+import Iphone14 from "../assets/Iphone14mini.png";
+import Iphone11 from "../assets/Iphone11mini.png";
+import Iphone11mini from "../assets/Iphone11mini-black.png";
+import CTA from "../assets/CTA.png";
+
+
+
+export default function Products() {
+
+  const product = [
+    {
+      image: Laptop,
+      name: "Laptop",
+    },
+    {
+      image: Iphone14,
+      name: "Iphone 14",
+  
+    },
+    {
+      image: Iphone11,
+      name: "Iphone 11",
+  
+    },
+    {
+      image: Iphone11mini,
+      name: "Iphone 11",
+
+    },
+    
+  ];
+
   return (
-    <div>CartSummary</div>
-  )
-}
+    <section className="">
+      
+    <h1 className="text-xl text-black font-bold mb-4">
+            Bag
+          </h1>
+          <div className="grid grid-cols-3 gap-4">
+            {product.map((product, index) => (
+        <div key={index} className=" w-[300px] h-[90px] gap-4 grid grid-cols-3 mb-4 ">
+          {/* Product header */}
+          
 
-export default CartSummary
+          {/* Product image */}
+          <img
+            src={product.image}
+            alt={product.name}
+            className="bg-white mini-cart object-contain  flex-wrap gap-4"
+          />
+
+          {/* Product details */}
+        </div>
+         
+         
+      ))}
+          </div>
+      
+      <img src={CTA} alt="cta-image" className="CTA" />
+    </section>
+  );
+}
