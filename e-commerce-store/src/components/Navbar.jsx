00041392c5c17d3e2e3fccb8020d0702d90f703b
mellow874 <felicitymncube49@gmail.com>
2025-11-14 12:05:49 +0200

    Centered Navbar

--- e-commerce-store/src/components/Navbar.jsx
+++ e-commerce-store/src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import react from 'react';
 export default function Navbar() {
     return(
         //Search item bar
-        <div className='gap-[8px] p-[8px] w-[523px] h-[99px]'>
-            <p className='text-rgba(96, 105, 92, 1) left-[8px] gap-[10px] right-[8px] text-[16px]'>
+        <div className='gap-4 p-8 items-center mb-15 w-[475px] h-6'>
+            <p className='text-description left-2 gap-2.5 right-2 text-[16px]'>
                 Search Item
             </p>
             
-            <input className='text-rgba(26, 31, 22, 0.5) rounded bg-white padding-[8px] gap-[10px] w-[475px] h-[40px]' 
+            <input className='text-rgba(26, 31, 22, 0.5) w-[475px] h-10 rounded bg-white p-2 gap-2.5' 
             type='text' 
-            placeholder='Apple Watch, Samsung S21, MacbookPro,...' />
+            placeholder='Apple Watch, Samsung S21, Macbook Pro, ...' />
         </div>
     );
 }
