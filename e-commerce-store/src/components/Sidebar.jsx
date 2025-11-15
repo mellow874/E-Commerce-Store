f1dcfc1e8447e1d979a83326ad42a4cd3edfcf40
mellow874 <felicitymncube49@gmail.com>
2025-11-16 00:56:42 +0200

    Added sidebar syntax and comments

--- e-commerce-store/src/components/Sidebar.jsx
+++ e-commerce-store/src/components/Sidebar.jsx
@@ -2,98 +2,98 @@ import React from "react";
 
 //Import images
 import Logo from "../assets/Logo.png";
-import Menu from "../assets/Menu.png";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
 import Bag from "../assets/Bag.png";
 import Store from "../assets/Store.png";
 import Logout from "../assets/Logout.png";
+import { createContext, useContext, useState } from 'react';
 
 
+const SidebarContext = createContext();
 
 export default function Sidebar() {
+  const [expanded, setExpanded] = useState(true);
+
   return (
-    <div className="flex flex-col items-center bg-gray-100 w-20 h-[992px] justify-between shadow-md">
-      {/* Top Logo */}
-      <a
-        href="/"
-        className="p-4  transition rounded-lg"
-        title="Logo"
-      >
+    <aside className='h-screen'>
+      <nav className="h-full min-h-full flex flex-col bg-white border-r shadow-sm ">
+        
+
+        {/* Sidebar items */}
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">
+            <SidebarItem src={Logo}  />
+            <div className=" flex justify-between items-center ml-3">
+          
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+          >
+            {expanded ? <RxCross2 size={20}/> : <RxHamburgerMenu size={20} />}
+          </button>
+        </div> 
+            <SidebarItem src={Store} text="Store"/>         
+            <SidebarItem src={Bag} text="Bag" />
+            
+            {/* Logout item positioned at the bottom */}
+            <div className=" mt-[500px] ml-[3px]">
+              <SidebarItem src={Logout} text="Logout" />
+            </div>
+            
+          </ul>
+        </SidebarContext.Provider>
+      </nav>
+    </aside>
+  );
+}
+
+export function SidebarItem({ src, icon, text, active, alert }) {
+  const { expanded } = useContext(SidebarContext);
+
+  return (
+    <li
+      className={`relative flex items-center py-4 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group
+        ${
+          active
+            
+        }`}
+    >
+
+      {/* Render image if provided */}
+      {src && (
         <img
-          src={Logo}
-          alt="Logo"
-          className="w-8 h-8 object-contain"
+          src={src}
+          alt={text}
+          className="w-8 h-auto object-contain"
         />
-      </a>
+      )}
 
-      {/* Nav Links */}
-      <ul className="flex flex-col space-y-4 mb-auto mt-6">
-        <li>
-          <a
-            href="#"
-            className="flex justify-center py-3 w-full  rounded-lg transition"
-            title="Menu"
-          >
-            <img src={Menu} alt="Menu" className="w-6 h-6" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="flex justify-center p-2 bg-black w-10 h-10 gap-2 rounded-lg transition"
-            title="Store"
-          >
-            <img src={Store} alt="Store" className="w-6 h-6" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="flex justify-center py-3 w-full  rounded-lg transition"
-            title="Products"
-          >
-            <img src={Bag} alt="Products" className="w-6 h-6 " />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="flex justify-center py-3 w-full  rounded-lg transition"
-            title="Customers"
-          >
-            <img src={Logout} alt="Customers" className="w-6 h-6" />
-          </a>
-        </li>
-      </ul>
+      {/* Render icon if provided */}
+      {icon && <span className="text-xl">{icon}</span>}
 
-      {/* Bottom Profile Dropdown */}
-      <div className="relative group border-t border-gray-200 w-full flex justify-center py-4">
-        {/* Dropdown */}
-        <ul className="absolute bottom-16 hidden group-hover:block bg-white shadow-lg rounded-lg text-sm w-40">
-          <li>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-              New project...
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-              Settings
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-              Profile
-            </a>
-          </li>
-          <li>
-            <hr className="my-1 border-gray-200" />
-          </li>
-          <li>
-            <a href="#" className="block px-4 py-2 hover:bg-gray-100">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <span
+        className={`overflow-hidden transition-all ${expanded ? 'w-20 ml-3' : 'w-0'}`}
+      >
+        {text}
+      </span>
+
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+            expanded ? '' : 'top-2'
+          }`}
+        />
+      )}
+
+      {!expanded && (
+        <div
+          className={`absolute left-full rounded-md px-2 ml-6 bg-indigo-100 text-sm invisible opacity-20 -translate-x-3
+            transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+        >
+          {text}
+        </div>
+      )}
+    </li>
   );
 }
