54c1cbdd5e3b214da045806b942276677c67adb8
Amkelwe Lubavu <lubavuamkelwe@gmail.com>
2025-11-17 01:40:20 +0200

    added  bag items and checkout content

--- e-commerce-store/src/App.jsx
+++ e-commerce-store/src/App.jsx
@@ -4,6 +4,10 @@ import Sidebar from './components/Sidebar';
 import BagItemscard1 from './components/BagItemscard1';
 import BagItemscard2 from './components/BagItemscard2';
 import BagItemscard3 from './components/BagItemscard3';
+import CheckOutcard1 from './components/CheckOutcard1';
+import CheckOutcard2 from './components/CheckOutcard2';
+import CheckOutcard3 from './components/CheckOutcard3';
+
 
 
 export default function App() {
@@ -11,7 +15,9 @@ export default function App() {
     <div className="flex bg-gray-200">
       {/* Sidebar component */}
       <Sidebar />
-      {/* Main content area */}
+
+      {/* bag items to appear on page 3 */}
+  
       <div className="flex flex-col p-4 gap-6">
           {/* Bag Items Card 1 */}
           <BagItemscard1/>
@@ -21,6 +27,14 @@ export default function App() {
 
        {/* Bag Items Card 3 */}
           <BagItemscard3/>
+
+
+          {/* checkout cards to appear on page 4 */}
+         
+
+          <CheckOutcard1/>
+             <CheckOutcard2/>
+                <CheckOutcard3/>
       </div>
 
       {/* Cart summary component */}
@@ -33,3 +47,8 @@ export default function App() {
     </div>
   );
 }
+
+
+//  add routing
+//responsiveness
+//add images on  top on bag item cards
